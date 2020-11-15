cedb18b6d6718b66b854b981215feac1fa3c1878
Henrique Tornelli Duarte <henrique.tornelli@ilinksolutions.com.br>
2020-11-15 19:33:27 -0300

    Implementando pub/sub

--- src/main/java/Publisher.java
+++ src/main/java/Publisher.java
@@ -1,2 +1,53 @@
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+
 public class Publisher {
+    public static void main(String args[]) {
+        String content;
+        String topic = "devices/sensor/temperature";
+        int qos = 1;
+        String broker = "tcp://mqtt.eclipse.org:1883";
+        String clientId = "JavaSample";
+        MemoryPersistence persistence = new MemoryPersistence();
+
+        try {
+            MqttClient sampleClient = new MqttClient(broker, clientId, persistence);
+            MqttConnectOptions connOpts = new MqttConnectOptions();
+            connOpts.setCleanSession(true);
+            System.out.println("Connecting to broker: "+ broker + "\n");
+
+            while (true) {
+                sampleClient.connect(connOpts);
+
+                content = Integer.toString(getRandomNumber(15, 45));
+
+                System.out.println("Publishing temp: " + content + "º");
+
+                MqttMessage message = new MqttMessage(content.getBytes());
+                message.setQos(qos);
+
+                sampleClient.publish(topic, message);
+
+                sampleClient.disconnect();
+
+                Thread.sleep(1000L);
+            }
+        } catch(MqttException e) {
+            System.out.println("reason " + e.getReasonCode());
+            System.out.println("msg " + e.getMessage());
+            System.out.println("loc " + e.getLocalizedMessage());
+            System.out.println("cause " + e.getCause());
+            System.out.println("excep " + e);
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static int getRandomNumber(int a, int b) {
+        return a + (int)(Math.random() * ((b - a) + 1));
+    }
 }
--- src/main/java/Subscriber.java
+++ src/main/java/Subscriber.java
@@ -1,2 +1,14 @@
-public class Subscriber {
+import org.eclipse.paho.client.mqttv3.*;
+
+public class Subscriber{
+    public static void main(String[] args) throws MqttException {
+        String topic = "devices/sensor/temperature";
+        String broker = "tcp://mqtt.eclipse.org:1883";
+        String clientId = "JavaSample2";
+        MqttClient sampleClient2 = new MqttClient(broker, clientId);
+
+        sampleClient2.setCallback(new SimpleMqttCallback());
+        sampleClient2.connect();
+        sampleClient2.subscribe(topic);
+    }
 }
